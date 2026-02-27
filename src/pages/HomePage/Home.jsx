49c9ba77f12701ae2c0cc387fca91a4ab76b9c8e
unknown <mrcplabon@gmail.com>
2026-02-28 00:45:34 +0600

    React router setup

---
+++ src/pages/HomePage/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <p className='coding text-4xl font-semibold'>This is Home</p>
+            <p className='moon text-4xl'>Will this be ever happen?</p>
+            <p className='ubuntu text-4xl'>And my name is Rafsan Chowdhury</p>
+            <p className='text-3xl font-bold'>And my name is Rafsan Chowdhury</p>
+        </div>
+    );
+};
+
+export default Home;

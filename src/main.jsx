6a188503346419fd6d11122be4a2d139480354c6
unknown <mrcplabon@gmail.com>
2026-03-04 21:32:52 +0600

    Added Firebase Authentication system + Created Auth context and provider

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import App from './App.jsx'
 import { RouterProvider } from 'react-router'
 import { router } from './router/router.jsx'
 import ThemeProvider from './context/Theme/ThemeProvider.jsx'
+import AuthProvider from './context/Auth/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
-      <div className=' min-h-screen transition-colors duration-300'>
-        <RouterProvider router={router} />
-      </div>
+      <AuthProvider>
+        <div className=' min-h-screen transition-colors duration-300'>
+          <RouterProvider router={router} />
+        </div>
+      </AuthProvider>
     </ThemeProvider>
   </StrictMode>,
 )
